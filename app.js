9f3a38e2b527386fe46efbd4bcad8b7a8091acce
surya-kiran27 <suryaraju67@gmail.com>
2020-04-13 20:22:56 +0530

    tst

--- app.js
+++ app.js
@@ -18,6 +18,29 @@ let express = require("express"),
 let passport = require("passport");
 var path = require("path");
 let LinkedInStrategy = require("passport-linkedin-oauth2").Strategy;
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use(cookieParser());
 
 app.use(bodyParser.urlencoded({ extended: false }));
