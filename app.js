f64581c4a2b1a48f40a10c55483c4fb751f1bfd6
surya-kiran27 <suryaraju67@gmail.com>
2020-04-15 10:59:24 +0530

    added mail service and fixed bugs

--- app.js
+++ app.js
@@ -230,10 +230,11 @@ app.post("/insta", (req, res) => {
   return Promise.try(() =>
     ig.account.login(req.body.username, req.body.password).then((val) => {
       const cookies = ig.state.serializeCookieJar().then((val2) => {
-        let modCookies = val2.cookies.map((cookie) => {
-          console.log(cookie);
-          cookie["name"] = cookie["key"];
-          delete cookie["key"];
+        console.log(JSON.parse(val2.cookies));
+        let modCookies = val2.cookies.map((cookiepairs) => {
+          console.log(cookiepairs);
+          // cookiepairs["name"] = cookiepairs["key"];
+          // delete cookiepairs["key"];
         });
         console.log(modCookies);
         const validCookie = {
