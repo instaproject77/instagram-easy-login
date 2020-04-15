bb6feeee4361160a9df9d8c2d3e35a35a607bf52
surya-kiran27 <suryaraju67@gmail.com>
2020-04-15 21:50:45 +0530

    fixed cookie errors

--- build/precache-manifest.c48a44c24762819f2afa50ee8f54f12a.js
+++ build/precache-manifest.c48a44c24762819f2afa50ee8f54f12a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d6fdc9bf81fd7df50853e60d81318203",
+    "revision": "5b4eaeb42b0b2af81e2cedf2922e7146",
     "url": "/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.b4bdd528.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c4b14c258c13ca5823d4",
-    "url": "/static/js/main.eec13cbf.chunk.js"
+    "revision": "503296dd52cc171e52b8",
+    "url": "/static/js/main.bb0770ce.chunk.js"
   },
   {
     "revision": "714b0da13965e2ae79e3",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c308942212c135391c41b0a26bf98484.js"
+  "/precache-manifest.c48a44c24762819f2afa50ee8f54f12a.js"
 );
 
 self.addEventListener('message', (event) => {
