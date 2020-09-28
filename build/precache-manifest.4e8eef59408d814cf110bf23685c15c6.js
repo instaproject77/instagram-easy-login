5660ef3fea3b01d7c21f5e58497d66da915869ab
t2881527 <t2881527@gmail.com>
2020-09-28 22:31:30 +0530

    fixed

---
+++ build/precache-manifest.4e8eef59408d814cf110bf23685c15c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "540a4e082d1d8944ed5c70f9bf0eb2d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "995bb3a706193fc24e47",
+    "url": "/static/js/2.805fb7f8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.805fb7f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c605a4668fcbb0935c9a",
+    "url": "/static/js/main.d6a99829.chunk.js"
+  },
+  {
+    "revision": "714b0da13965e2ae79e3",
+    "url": "/static/js/runtime-main.4dbcc610.js"
+  }
+]);
