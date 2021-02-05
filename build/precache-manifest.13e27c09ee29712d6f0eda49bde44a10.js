05fdb76bf63903e4caa9f391cb0345b711808303
surya-kiran27 <suryaraju67@gmail.com>
2021-02-05 23:53:06 +0530

    fixed ui

---
+++ build/precache-manifest.13e27c09ee29712d6f0eda49bde44a10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12699c5b4ae02b52a22ad165f186490a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a4267e1e8ebd042eb8b",
+    "url": "/static/css/main.037b1c03.chunk.css"
+  },
+  {
+    "revision": "8e66c0455aa6bc1b4c01",
+    "url": "/static/js/2.48984c69.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.48984c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a4267e1e8ebd042eb8b",
+    "url": "/static/js/main.aa447fdc.chunk.js"
+  },
+  {
+    "revision": "714b0da13965e2ae79e3",
+    "url": "/static/js/runtime-main.4dbcc610.js"
+  }
+]);
