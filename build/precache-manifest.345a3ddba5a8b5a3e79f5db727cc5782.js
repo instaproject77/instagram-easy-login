04043b4013999ea6e50a10d8853f789dac5ddb29
Anish Ojha <anish.ojha@provistechnologies.com>
2021-06-07 19:23:45 +0530

    minor build fix

---
+++ build/precache-manifest.345a3ddba5a8b5a3e79f5db727cc5782.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01296ee39afad97c88315aa59955251c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82e437d0d7cd032540a0",
+    "url": "/static/css/main.68bffd5a.chunk.css"
+  },
+  {
+    "revision": "b06ba9abc0cb3d4347a8",
+    "url": "/static/js/2.f413ed74.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f413ed74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82e437d0d7cd032540a0",
+    "url": "/static/js/main.6543be02.chunk.js"
+  },
+  {
+    "revision": "714b0da13965e2ae79e3",
+    "url": "/static/js/runtime-main.4dbcc610.js"
+  },
+  {
+    "revision": "c888480a77d1b50b973232c0f0bda889",
+    "url": "/static/media/facebook-logo.c888480a.svg"
+  },
+  {
+    "revision": "c1ea2611df7286a533b357c20658853e",
+    "url": "/static/media/instagram-logo.c1ea2611.svg"
+  },
+  {
+    "revision": "ab5b573d0c2c0e4457467dbd69b85137",
+    "url": "/static/media/linkedin-logo.ab5b573d.svg"
+  },
+  {
+    "revision": "c6b5b406d763cb3220f9cc10b87c4969",
+    "url": "/static/media/logo.c6b5b406.png"
+  },
+  {
+    "revision": "cab4ec596b9c7fa025f52b25bc91b49e",
+    "url": "/static/media/pinterest-logo.cab4ec59.svg"
+  },
+  {
+    "revision": "cde0083c588e107011c7a0d75e235db4",
+    "url": "/static/media/twitter-logo.cde0083c.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2133adaf85e0176b5b5c604778d4b8df.js"
+  "/precache-manifest.345a3ddba5a8b5a3e79f5db727cc5782.js"
 );
 
 self.addEventListener('message', (event) => {
